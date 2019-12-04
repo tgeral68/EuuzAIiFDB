9dcff46c5a2ee3a3736c75f8c115a18777479ca4
gerald <thomas.l.p.gerald@gmail.com>
2019-12-05 00:06:37 +0100

    update experiment script

--- script/example_dblp.sh
+++ script/example_dblp.sh
@@ -1,5 +1,5 @@
 # running experiments if no cuda device (Nvidia GPU) please remove --cuda parameter if no GPU we recommend you to make smallest batch/learning rate and less iteration
-CUDA_VISBLE_DEVICES=0 python3.7 experiments_launchers/experiment_poincare.py  --cuda  --batch-size 512 --dataset dblp  --n-centroid 5 --walk-lenght 60  --precompute-rw 6 --beta 1.  --alpha .5 --lr 25.  --negative-sampling 8  --context-size 10  --epoch 1000 --embedding-optimizer exphsgd   --id "dblp-2D" --size 2 --seed 120
+CUDA_VISBLE_DEVICES=0 python3.7 experiments_launchers/experiment_poincare.py  --cuda  --batch-size 512 --dataset dblp  --n-centroid 5 --walk-lenght 60  --precompute-rw 6 --beta 1.  --alpha .5 --lr 25.  --negative-sampling 8  --context-size 10  --epoch 300 --embedding-optimizer exphsgd   --id "dblp-2D" --size 2 --seed 120
 
 # create visualization unsupervised/supervised and ground truth
 python3.7  experiments_launchers/visualisation_poincare_kmeans.py --id 'dblp-2D'
